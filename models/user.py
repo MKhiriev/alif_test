322c0abd1a5ec23cce738a016a6e09199d4eab28
Khiriev.MKh <khiriev.rasul@inbox.ru>
2025-02-07 22:27:48 +0500

    Added User methods: - get_contact_info() - __str__()

--- models/user.py
+++ models/user.py
@@ -4,3 +4,9 @@ class User:
         self.name = name
         self.email = email
         self.telephone = telephone
+
+    def get_contact_info(self):
+        return {'telephone': self.telephone, 'email': self.email}
+
+    def __str__(self):
+        return f"ID: {self.user_id}. {self.name} Contacts: {self.email} {self.telephone}"
