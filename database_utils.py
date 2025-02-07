8ae2264bfb1234696434a91579942206f32144eb
Khiriev.MKh <khiriev.rasul@inbox.ru>
2025-02-07 20:50:15 +0500

    Created method stub for preparing database

---
+++ database_utils.py
@@ -0,0 +1,12 @@
+def prepare_db():
+    """
+    Prepares database for booking app.
+
+    :return: None
+    """
+    # 1. establish database connection
+    # 2. create tables
+    # 3. insert test data
+    # 4. commit changes
+    # 5. close database connection
+    pass
