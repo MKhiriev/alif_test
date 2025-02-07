c34bba52fc1f7494e2750a9ccac05c0ce8f732a7
Khiriev.MKh <khiriev.rasul@inbox.ru>
2025-02-07 22:55:55 +0500

    Added docs for Booking class methods

--- models/booking.py
+++ models/booking.py
@@ -10,9 +10,20 @@ class Booking:
         self.unix_datetime_end = unix_datetime_end
 
     def duration(self):
+        """
+        Returns the duration of the booking in seconds.
+
+        :return: integer duration in unix format
+        """
         return self.unix_datetime_end - self.unix_datetime_start
 
     def is_active(self, current_time):
+        """
+        Returns true if the booking is active during given time.
+
+        :param current_time: time in seconds
+        :return:
+        """
         return self.unix_datetime_start <= current_time <= self.unix_datetime_end
 
     def __str__(self):
