7ddcc0ea8348829094e908a6bf8f1d83aeea1ae4
Khiriev.MKh <khiriev.rasul@inbox.ru>
2025-02-07 18:27:52 +0500

    Added docstring to all methods (including stubs)

--- main.py
+++ main.py
@@ -13,28 +13,58 @@
 
 
 def check_if_available(room_id, booking_time):
+    """
+    Checks room availability by booking time.
+
+    :param room_id: id of room which is going to be booked
+    :param booking_time: list with two elements [datetime_start, datetime_end]
+    :return: list with two elements - [if_available, overlapping_bookings]
+    """
     pass
 
 
 def add_room_booking(user_id, room_id, booking_time):
+    """
+    Adds room booking to the database.
+
+    :param user_id: id of user who is going to book
+    :param room_id: id of room which is going to be booked
+    :param booking_time: list with two elements [datetime_start, datetime_end]
+    :return: booking id of added booking
+    """
     pass
 
 
 def notify_user(user_id, booking_info):
+    """
+    Notifies user with info about created booking.
+
+    :param user_id: id of user who is going to book
+    :param booking_info: booking info which is going to be printed
+    :return: None
+    """
     pass
 
 
 def print_booking_info(booking):
+    """
+    Prints booking info to the user.
+
+    :param booking: booking information which is going to be printed
+    :return: None
+    """
     pass
 
 
-"""
-To book a room we need user, room and booking time.
-List of arguments: user_id, room_id, booking_time
-"""
-
-
 def book_room(user_id, room_id, booking_time):
+    """
+    Books a room.
+
+    :param user_id: id of user who is going to book
+    :param room_id: id of room which is going to be booked
+    :param booking_time: list with two elements [datetime_start, datetime_end]
+    :return: None
+    """
     # Check if room available. Use room number and booking time (start, end).
     [available, overlapping_bookings] = check_if_available(room_id, booking_time)
     # if room is available then add booking and notify person by email and phone number
