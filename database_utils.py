9755075329d4b41109218af53547ac2e11e95e31
Khiriev.MKh <khiriev.rasul@inbox.ru>
2025-02-07 20:55:55 +0500

    - Implemented prepare_db() - Declared necessary stubs

--- database_utils.py
+++ database_utils.py
@@ -1,3 +1,26 @@
+import sqlite3
+
+
+def create_tables(cursor):
+    """
+    Create tables in database.
+
+    :param cursor: sqlite3.Cursor object
+    :return: None
+    """
+    pass
+
+
+def insert_data(cursor):
+    """
+    Insert testing data in database.
+
+    :param cursor: sqlite3.Cursor object
+    :return: None
+    """
+    pass
+
+
 def prepare_db():
     """
     Prepares database for booking app.
@@ -5,8 +28,15 @@ def prepare_db():
     :return: None
     """
     # 1. establish database connection
+    conn = sqlite3.connect('example.db')
+    cursor = conn.cursor()
+
     # 2. create tables
+    create_tables(cursor)
     # 3. insert test data
+    insert_data(cursor)
+
     # 4. commit changes
+    conn.commit()
     # 5. close database connection
-    pass
+    conn.close()
