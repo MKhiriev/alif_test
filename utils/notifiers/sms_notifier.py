2ca288e563da2bf7bf7c24524f2a92501b7fbcfb
Khiriev.MKh <khiriev.rasul@inbox.ru>
2025-02-08 00:13:50 +0500

    Created email and sms notifiers

---
+++ utils/notifiers/sms_notifier.py
@@ -0,0 +1,10 @@
+from utils.notifiers.notifier import Notifier
+
+
+class SmsNotifier(Notifier):
+    def __init__(self, sms_provider):
+        super().__init__()
+        self.sms_provider = sms_provider
+
+    def send_notification(self, recipient, message):
+        print(f"Sending SMS to {recipient}: {message}")
